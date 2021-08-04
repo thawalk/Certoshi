cbc97168d8012d7c60036f626c3cddd099d6731f
Daryll Wong <daryll_wong@hotmail.com>
2021-08-04 12:55:37 +0800

    update deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,14 @@
 var Certification = artifacts.require("./Certification.sol");
+var Institution = artifacts.require("./Institution.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Certification);
+module.exports = async function(deployer) {
+  // deployer.deploy(Certification);
+
+  // Deploy Institution Contract
+  await deployer.deploy(Institution)
+  const institution = await Institution.deployed()
+
+  // Deploy Certification Contract
+  await deployer.deploy(Certification, institution.address)
+  const certification = await Certification.deployed()
 };
