26186e9a2f4aef20afe1448fe8f1c7e407a7a68b
thawalk <tharunana@gmail.com>
2021-08-10 03:43:50 +0800

    more interfacing

--- client/src/Components/Admin.js
+++ client/src/Components/Admin.js
@@ -2,8 +2,7 @@ import React from "react";
 import Institution from '../contracts/Institution.json'
 import Web3 from 'web3'
 import { v4 as uuidv4 } from 'uuid';
-import contract from 'truffle-contract'
-const InstitutionInstance = contract(Institution)
+import TextField from '@material-ui/core/TextField';
 
 class Admin extends React.Component {
     state = {
@@ -12,9 +11,14 @@ class Admin extends React.Component {
         institution: {},
         renderLoading: true,
         renderAdmin: false,
-        renderMetaMaskError: false
-
-
+        renderMetaMaskError: false,
+        instituteAddress: "",
+        instituteName: "",
+        instituteAcronym: "",
+        instituteWebsite: "",
+        course: "",
+        // instituteCourses: new Array()
+        instituteCourses: []
     };
 
     async componentWillMount() {
@@ -45,8 +49,6 @@ class Admin extends React.Component {
         const web3 = window.web3
         try {
             const accounts = await web3.eth.getAccounts()
-            console.log('checking accounts')
-            console.log(accounts)
             let caller = accounts[0]
 
             const networkId = await web3.eth.net.getId()
@@ -106,86 +108,235 @@ class Admin extends React.Component {
         const accounts = await web3.eth.getAccounts()
         let caller = accounts[0]
 
-        let unique_id = uuidv4()
-        console.log(unique_id)
-        let mockInstituteCourses = [{
-            course_name: "Computer Science and Design",
-        },
-        {
-            course_name: "Engineering Product and Development",
-        },
-        {
-            course_name: "Engineering Systems and Design",
-        },
-        {
-            course_name: "Architecture and Sustainable Design",
-        },
-        ];
+        //------------ mock data -----------------//
+        // let instituteAddress = uuidv4()
 
-        let address = unique_id;
-        let mockInstitute = {
-            instituteName: "Singapore University of Technology and Design",
-            instituteAcronym: "SUTD",
-            instituteLink: "https://sutd.edu.sg/",
-        };
+        // let instituteAddress = "0x83E41c66E7EE0f14d0Fc8E74720652F6662eB1Eb"
 
+        // let mockInstituteCourses = [{
+        //     course_name: "Computer Science and Design",
+        // },
+        // {
+        //     course_name: "Engineering Product and Development",
+        // },
+        // {
+        //     course_name: "Engineering Systems and Design",
+        // },
+        // {
+        //     course_name: "Architecture and Sustainable Design",
+        // },
+        // ];
+
+        // let mockInstitute = {
+        //     instituteName: "Singapore University of Technology and Design",
+        //     instituteAcronym: "SUTD",
+        //     instituteLink: "https://sutd.edu.sg/",
+        // };
+        //------------ mock data -----------------//
+
+        // instantiate the contract (---can't maintain it in a state for some reason, need to check again later----)
         const networkId = await web3.eth.net.getId()
         const institutionData = Institution.networks[networkId]
         const institution = new web3.eth.Contract(Institution.abi, institutionData.address)
-
-        console.log(institution)
         try {
             // get owner of smart contract
             let smartContractOwner = await institution.methods.owner().call()
 
             // compare the caller and the owner of smart contract
             if (caller == smartContractOwner) {
+
+                //------------ mock data -----------------//
+                // await institution.methods.addInstitute(
+                //     instituteAddress,
+                //     mockInstitute.instituteName,
+                //     mockInstitute.instituteAcronym,
+                //     mockInstitute.instituteLink,
+                //     mockInstituteCourses
+                // )
+                //------------ mock data -----------------//
+
                 await institution.methods.addInstitute(
-                    address,
-                    mockInstitute.instituteName,
-                    mockInstitute.instituteAcronym,
-                    mockInstitute.instituteLink,
-                    mockInstituteCourses
+                    this.state.instituteAddress,
+                    this.state.instituteName,
+                    this.state.instituteAcronym,
+                    this.state.instituteWebsite,
+                    this.state.instituteCourses
                 )
-                .send({ from: caller }).on('receipt', function (receipt) {
-                    console.log(receipt);
-                    console.log(receipt.events)
-                })
+                    .send({ from: caller }).on('receipt', function (receipt) {
+                        console.log(receipt);
+                        console.log(receipt.events)
+                        //-------clearing the value, doesn't work, check this please----------//
+                        // this.setState({
+                        //     instituteAddress:"",
+                        //     instituteName:"",
+                        //     instituteAcronym: "",
+                        //     instituteWebsite: "",
+                        //     course: ""
+                        // })
+
+                        // ----- here can use a state or smth, to display a success message -----
+                    })
             }
             else {
                 window.alert('Not the account used to deploy smart contract')
             }
         }
         catch (error) {
+            console.log(error)
             console.log(error.code)
-            if (error.code == 4001) {
+            if (error.code == -32603) {
+                window.alert('Institute account already exits')
+            }
+            else if (error.code == 4001) {
                 window.alert('Transaction rejected')
             }
+            else {
+                window.alert('Institute account address is not legit')
+            }
+        
         }
     }
 
 
-    render() {
-        const {
-            renderLoading,
-            renderAdmin,
-            renderMetaMaskError,
-        } = this.state;
-        return (
-            <>
-                {
-                    renderLoading ? (<h1>Loading</h1>) :
-                        renderMetaMaskError ? (<h1>Metamask issue</h1>) :
-                            renderAdmin ? (<h1>Access granted</h1>) :
-                                (<h1>Not admin</h1>)
+check() {
+    console.log(this.state.instituteCourses)
+}
+handleTextFieldChangeAddress(e) {
+    this.setState({
+        instituteAddress: e.target.value
+    })
+}
 
-                }
-                {renderAdmin ? <button onClick={this.addInstituteToBlockchain}>
-                    Add institute
-                </button>: <></>}
-            </>
-        );
-    }
+handleTextFieldChangeName(e) {
+    this.setState({
+        instituteName: e.target.value
+    })
+}
+handleTextFieldChangeAcronym(e) {
+    this.setState({
+        instituteAcronym: e.target.value
+    })
+}
+handleTextFieldChangeWebsite(e) {
+    this.setState({
+        instituteWebsite: e.target.value
+    })
+}
+handleTextFieldChangeCourse(e) {
+    this.setState({
+        course: e.target.value
+    })
+}
+
+appendToCourseList() {
+    this.setState({
+        instituteCourses: [...this.state.instituteCourses, { course_name: this.state.course }],
+        course: ""
+    })
+    console.log(this.state.instituteCourses)
+}
+
+
+render() {
+    const {
+        renderLoading,
+        renderAdmin,
+        renderMetaMaskError,
+        course,
+        instituteWebsite,
+        instituteAddress,
+        instituteAcronym,
+        instituteName
+    } = this.state;
+    return (
+        <>
+            {
+                renderLoading ? (<h1>Loading</h1>) :
+                    renderMetaMaskError ? (<h1>Metamask issue</h1>) :
+                        renderAdmin ? (<h1>Access granted</h1>) :
+                            (<h1>Not admin</h1>)
+
+            }
+            {renderAdmin ?
+                <>
+
+                    <TextField
+                        // add a error function to ensure they don't submit empty strings
+                        // error={error}
+                        autoFocus
+                        margin="dense"
+                        id="address"
+                        label="Institute Account Address"
+                        type="name"
+                        value={instituteAddress}
+                        fullWidth
+                        // helperText={helperText}
+                        onChange={(e) => this.handleTextFieldChangeAddress(e)}
+                    />
+                    <TextField
+                        // add a error function to ensure they don't submit empty strings
+                        // error={error}
+                        autoFocus
+                        margin="dense"
+                        id="address"
+                        label="Institute Name"
+                        type="name"
+                        value={instituteName}
+                        fullWidth
+                        // helperText={helperText}
+                        onChange={(e) => this.handleTextFieldChangeName(e)}
+                    />
+                    <TextField
+                        // add a error function to ensure they don't submit empty strings
+                        // error={error}
+                        autoFocus
+                        margin="dense"
+                        id="address"
+                        label="Institute Acronym"
+                        type="name"
+                        value={instituteAcronym}
+                        fullWidth
+                        // helperText={helperText}
+                        onChange={(e) => this.handleTextFieldChangeAcronym(e)}
+                    />
+                    <TextField
+                        // add a error function to ensure they don't submit empty strings
+                        // error={error}
+                        autoFocus
+                        margin="dense"
+                        id="address"
+                        label="Institute Website"
+                        type="name"
+                        value={instituteWebsite}
+                        fullWidth
+                        // helperText={helperText}
+                        onChange={(e) => this.handleTextFieldChangeWebsite(e)}
+                    />
+                    <TextField
+                        // add a error function to ensure they don't submit empty strings
+                        // error={error}
+                        autoFocus
+                        margin="dense"
+                        id="address"
+                        label="Add course"
+                        type="name"
+                        fullWidth
+                        value={course}
+                        // helperText={helperText}
+                        onChange={(e) => this.handleTextFieldChangeCourse(e)}
+                    />
+                    <button onClick={() => this.appendToCourseList()}>
+                        Add Course
+                    </button>
+                    <button onClick={() => this.addInstituteToBlockchain()}>
+                        Add institute
+                    </button>
+                </> : <></>}
+
+
+        </>
+    );
+}
 }
 
 export default Admin
--- client/src/Components/GenerateCert.js
+++ client/src/Components/GenerateCert.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Institution from '../contracts/Institution.json'
+import Certification from '../contracts/Certification.json'
 import Web3 from 'web3'
 import { v4 as uuidv4 } from 'uuid';
 
@@ -26,7 +27,6 @@ class GenerateCert extends React.Component {
     async loadWeb3Metamask() {
         if (window.ethereum) {
             window.web3 = new Web3(window.ethereum)
-            console.log("passing")
             await window.ethereum.enable().then(res => {
                 this.setState({
                 renderLoading: false,
@@ -36,7 +36,6 @@ class GenerateCert extends React.Component {
         }
         else if (window.web3) {
             window.web3 = new Web3(window.web3.currentProvider)
-            console.log("passing as well")
             this.setState({
                 renderLoading: false,
                 renderMetaMaskError: false
@@ -61,23 +60,108 @@ class GenerateCert extends React.Component {
         const institutionData = Institution.networks[networkId]
         const institution = new web3.eth.Contract(Institution.abi, institutionData.address)
 
-        let mockToken = '0f747a16-7c3a-4c98-9e1a-c4519cdb0682'
+        // let instituteAddress = "0x83E41c66E7EE0f14d0Fc8E74720652F6662eB1Eb"
 
-        await institution.methods.getInstituteData(mockToken).call().then(res => {
-            const formattedInstituteCoursesData = res[3].map((x) => {
-                return { course_name: x.course_name };
-            });
-
-            this.setState({
-                instituteName: res[0],
-                instituteAcronym: res[1],
-                instituteWebsite: res[2],
-                instituteCourses: formattedInstituteCoursesData
+        let instituteAddress = "0x2C7475d55Fa3e0F239e244846CCe297fdeB3D0F3"
+        try{
+            
+            await institution.methods.getInstituteData(instituteAddress).call().then(res => {
+                const formattedInstituteCoursesData = res[3].map((x) => {
+                    return { course_name: x.course_name };
+                });
+    
+                this.setState({
+                    instituteName: res[0],
+                    instituteAcronym: res[1],
+                    instituteWebsite: res[2],
+                    instituteCourses: formattedInstituteCoursesData
+                })
             })
-        })
+        }
+        catch (error) {
+            alert("Account address is wrong or does not exist in the smart contract")
+        }
 
 
     } 
+
+    async generateCertificate() {
+        console.log("adding institute to the blockchain")
+        const web3 = window.web3
+        const accounts = await web3.eth.getAccounts()
+        let caller = accounts[0]
+
+        //------------ mock data -----------------//
+        let mockCert = {
+            candidateName: "John Lim",
+            orgName: "Singapore University of Technology and Design",
+            courseName: "Computer Science and Design",
+            expirationDate: new Date().getTime(),
+            id: "5c0157fd3ff47a2a54075b01",
+        };
+        //------------ mock data -----------------//
+
+        // instantiate the contract (---can't maintain it in a state for some reason, need to check again later----)
+        const networkId = await web3.eth.net.getId()
+        const institutionData = Institution.networks[networkId]
+        const institution = new web3.eth.Contract(Institution.abi, institutionData.address)
+        try {
+            // get owner of smart contract
+            let smartContractOwner = await institution.methods.owner().call()
+
+            // compare the caller and the owner of smart contract
+            if (caller == smartContractOwner) {
+
+                //------------ mock data -----------------//
+                // await institution.methods.addInstitute(
+                //     instituteAddress,
+                //     mockInstitute.instituteName,
+                //     mockInstitute.instituteAcronym,
+                //     mockInstitute.instituteLink,
+                //     mockInstituteCourses
+                // )
+                //------------ mock data -----------------//
+
+                await institution.methods.addInstitute(
+                    this.state.instituteAddress,
+                    this.state.instituteName,
+                    this.state.instituteAcronym,
+                    this.state.instituteWebsite,
+                    this.state.instituteCourses
+                )
+                    .send({ from: caller }).on('receipt', function (receipt) {
+                        console.log(receipt);
+                        console.log(receipt.events)
+                        //-------clearing the value, doesn't work also----------//
+                        // this.setState({
+                        //     instituteAddress:"",
+                        //     instituteName:"",
+                        //     instituteAcronym: "",
+                        //     instituteWebsite: "",
+                        //     course: ""
+                        // })
+
+                        // ----- here can use a state or smth, to display a success message -----
+                    })
+            }
+            else {
+                window.alert('Not the account used to deploy smart contract')
+            }
+        }
+        catch (error) {
+            console.log(error)
+            console.log(error.code)
+            if (error.code == -32603) {
+                window.alert('Institute account already exits')
+            }
+            if (error.code == 4001) {
+                window.alert('Transaction rejected')
+            }
+        }
+    }
+
+
+
 
     render() {
         const {
@@ -101,6 +185,9 @@ class GenerateCert extends React.Component {
                 <h1>{instituteWebsite}</h1>
                 {/*need to pipe properly, but the data is in instituteCourses*/}
                 {/* <h1>{instituteCourses}</h1> */}
+                <button onClick={() => this.generateCertificate()}>
+                    Generate Cert
+                </button>
             </>
         );
     }
